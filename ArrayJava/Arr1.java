5285f7c9743861dd4527823b5fd803e93e7f363c
nithi35 <66963638+nithi35@users.noreply.github.com>
2020-06-19 07:40:26 +0530

    Add files via upload

---
+++ ArrayJava/Arr1.java
@@ -0,0 +1,20 @@
+public class Arr1{
+public static void main(String args[]){
+	int[] arr=new int[args.length];
+	int avg=0;
+	int sum=0;
+	int n=arr.length;
+	for(int i=0;i<args.length;i++)
+		{
+		   arr[i]=Integer.parseInt(args[i]);
+		}
+	for(int i=0;i<n;i++)
+	{
+		sum=sum+arr[i];
+		avg=sum/n;
+	}
+	System.out.println("Sum  "+sum);
+	System.out.println("Average  "+avg);
+	}
+}
+			
